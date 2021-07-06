26ce14af8d9aa234136e68cf221369f87b612f97
Antony Gaetano <antonygaetano1000@gmail.com>
2021-07-06 12:46:55 -0300

    Atualização no Html, Css e Javascript

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 var ID;
-var tempo = 9;
+var tempo = 5;
 var pontoA = 0;
 var pontoB = 0;
 
@@ -16,6 +16,7 @@ for(let i = 0; i < 3; i++){
 
 var tag = document.getElementById(id);
 tag.style.transform="scale(1.3)";
+
 }
 
 function Tempo(){
@@ -24,7 +25,8 @@ div.innerHTML = "00:0" + tempo;
 if(tempo == 0){
   clearInterval(tmp);
   Jogar();
-  tempo=10;
+  tempo=6;
+  setTimeout(inicia,1000);
 }
 tempo--;
 }
@@ -32,7 +34,6 @@ tempo--;
 function inicia(){
   tmp = setInterval(Tempo,800);
 }
-
 
 function Jogar(){
 var vetor_Pc2 = ["mao-pc","pedra-pc","tesoura-pc"];
@@ -60,35 +61,15 @@ var divjogB = document.getElementById("PontuacaoB");
 divjogA.innerHTML = pontoA;
 divjogB.innerHTML = pontoB;
 
-if(nomePC == "pedra-pc" && nomeJog == "mao-jog" || nomePC == "tesoura-pc" && nomeJog == "pedra-jog" || nomePC == "mao-pc" && nomeJog == "tesoura-jog")
-{
+if(nomePC == "pedra-pc" && nomeJog == "mao-jog" || nomePC == "tesoura-pc" && nomeJog == "pedra-jog" || nomePC == "mao-pc" && nomeJog == "tesoura-jog"){
   pontoA++;
 }
-else if(nomePC == "pedra-pc" && nomeJog == "pedra-jog" || nomePC == "tesoura-pc" && nomeJog == "tesoura-jog" || nomePC == "mao-pc" && nomeJog == "mao-jog"){
-  
-}
+else if(nomePC == "pedra-pc" && nomeJog == "pedra-jog" || nomePC == "tesoura-pc" && nomeJog == "tesoura-jog" || nomePC == "mao-pc" && nomeJog == "mao-jog"){/*Pontuação para nenhum lado*/}
 else{
     pontoB++;
 }
 
 divjogA.innerHTML = pontoA;
 divjogB.innerHTML = pontoB;
-
-
-var vetor_Jog2 = ["mao-jog","pedra-jog","tesoura-jog"];
-var nome1 = [];
-
-for(let i = 0; i < 3; i++){
- nome1[i] = document.getElementById(vetor_Jog2[i]);
- nome1[i].style.transform="none";
-} 
-
-var vetor_Jog = ["mao-jog","pedra-jog","tesoura-jog"];
-var nome = [];
-
-for(let i = 0; i < 3; i++){
- nome[i] = document.getElementById(vetor_Jog[i]);
- nome[i].style.transform="none"
-} 
 
 }
